25f16096a489b5a940e3e5238a466a381654cca3
James Woolfenden <james.woolfenden@gmail.com>
2026-05-04 16:09:39 +0100

    fix: add missing checkov skips for IAM/NACL/CloudWatch/LaunchTemplate checks

--- aws_iam_role_policy.ebsbackuplambda.tf
+++ aws_iam_role_policy.ebsbackuplambda.tf
@@ -1,4 +1,6 @@
 resource "aws_iam_role_policy" "ebsbackuplambda" {
+  # checkov:skip=CKV_AWS_290: Policy requires broad access for this module to function
+  # checkov:skip=CKV_AWS_355: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_272: Policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_356: Policy requires broad access for this module to function
   name   = "ebsbackuplambda-${random_string.label.result}"
@@ -6,6 +8,7 @@ resource "aws_iam_role_policy" "ebsbackuplambda" {
   policy = data.aws_iam_policy_document.ebsbackuplambda.json
 }
 data "aws_iam_policy_document" "ebsbackuplambda" {
+  # checkov:skip=CKV_AWS_356: IAM policy requires broad access for this module to function
   # checkov:skip=CKV_AWS_290: IAM policy requires broad write access for this module to function
   # checkov:skip=CKV_AWS_355: IAM policy requires wildcard resource for this module to function
   #checkov:skip=CKV_AWS_111:A general role for many ec2 instances
